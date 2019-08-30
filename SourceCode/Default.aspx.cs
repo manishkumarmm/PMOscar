85c57e1ded64246562157caf4bf6d7574738876d
Ann <ann.mariya@naicotech.com>
2019-08-30 11:46:54 +0530

    g suite on off flag added.

--- SourceCode/Default.aspx.cs
+++ SourceCode/Default.aspx.cs
@@ -19,89 +19,78 @@ public partial class Default : System.Web.UI.Page
     {
         lblmsg.Text = "";
         txtUserName.Focus();
-
         try
         {
-            string logOut = Request.QueryString["action"];
-            if (logOut != null && logOut != "")
+            string encryptString = Request.QueryString["data"];
+            string Email = Request.QueryString["Email"];
+            var json_serializer = new JavaScriptSerializer();
+            bool gsuite = false;
+            if (encryptString != null && encryptString != "")
             {
-                Session["UserName"] = null;
-                Session.Abandon();
+                gsuite = true;
+                var request = (HttpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["gSuiteKey"]);
+                var response = (HttpWebResponse)request.GetResponse();
+                var dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content fully up to the end.
+                string secretKey = reader.ReadToEnd();
+
+                TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+                byte[] byteBuff;
+                string iv = Request.QueryString["iv"];
+                desCryptoProvider.Key = Encoding.UTF8.GetBytes(secretKey);
+                desCryptoProvider.IV = UTF8Encoding.UTF8.GetBytes(iv);
+                byteBuff = Convert.FromBase64String(encryptString);
+
+                string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+
+                var gsuiteData = (IDictionary<string, object>)json_serializer.DeserializeObject(plaintext);
+                Email = gsuiteData["email"].ToString();
             }
-            else
+            if (Email != null)
             {
-                string encryptString = Request.QueryString["data"];
-                string Email = Request.QueryString["Email"];
-                var json_serializer = new JavaScriptSerializer();
-                bool gsuite = false;
-                if (encryptString != null && encryptString != "")
+                string Password = "";
+                if (!gsuite)
                 {
-                    gsuite = true;
-                    var request = (HttpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["gSuiteKey"]);
-                    var response = (HttpWebResponse)request.GetResponse();
-                    var dataStream = response.GetResponseStream();
-                    StreamReader reader = new StreamReader(dataStream);
-
-                    // Read the content fully up to the end.
-                    string secretKey = reader.ReadToEnd();
-
-                    TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
-                    byte[] byteBuff;
-                    string iv = Request.QueryString["iv"];
-                    desCryptoProvider.Key = Encoding.UTF8.GetBytes(secretKey);
-                    desCryptoProvider.IV = UTF8Encoding.UTF8.GetBytes(iv);
-                    byteBuff = Convert.FromBase64String(encryptString);
-
-                    string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
-
-                    var gsuiteData = (IDictionary<string, object>)json_serializer.DeserializeObject(plaintext);
-                    Email = gsuiteData["email"].ToString();
+                    Password = PMOscar.BaseDAL.DecryptString(Request.QueryString["Password"].Replace(" ", "+"));
                 }
-                if (Email != null)
+                string returnUrl = Request.QueryString["returnUrl"];
+                var user = new User();
+                var objUserDAL = new UserDAL();
+
+                if (objUserDAL.CheckUserAuthentication(Email, PMOscar.BaseDAL.EncryptText(Password), out user, gsuite))
                 {
-                    string Password = "";
-                    if (!gsuite)
+                    if (!user.IsActive)
                     {
-                        Password = PMOscar.BaseDAL.DecryptString(Request.QueryString["Password"].Replace(" ", "+"));
-                    }
-                    string returnUrl = Request.QueryString["returnUrl"];
-                    var user = new User();
-                    var objUserDAL = new UserDAL();
-
-                    if (objUserDAL.CheckUserAuthentication(Email, PMOscar.BaseDAL.EncryptText(Password), out user, gsuite))
-                    {
-                        if (!user.IsActive)
-                        {
-                            lblmsg.Text = "This user account has been disabled!";
-                        }
-                        else
-                        {
-
-                            Session[Constants.SessionName.USERNAME] = user.UserName;
-                            Session[Constants.SessionName.ENCRYPTEDPASSWORD] = PMOscar.BaseDAL.EncryptString(Password);
-                            Session[Constants.SessionName.USERID] = user.UserId;
-                            Session[Constants.SessionName.USERROLEID] = user.UserRole.UserRoleId;
-                            lblmsg.Text = "";
-                            if (gsuite)
-                            {
-                                if (Convert.ToInt32(Session[Constants.SessionName.USERROLEID]) == 4)
-                                    Response.Redirect("UserListing.aspx", false);
-                                else
-                                    Response.Redirect("ResourcePlanning.aspx", false);
-                            }
-                            else Response.Redirect(returnUrl);
-                        }
+                        lblmsg.Text = "This user account has been disabled!";
                     }
                     else
                     {
-                        lblmsg.Text = "Invalid UserName/Password";
+                        Session[Constants.SessionName.USERNAME] = user.UserName;
+                        Session[Constants.SessionName.ENCRYPTEDPASSWORD] = PMOscar.BaseDAL.EncryptString(Password);
+                        Session[Constants.SessionName.USERID] = user.UserId;
+                        Session[Constants.SessionName.USERROLEID] = user.UserRole.UserRoleId;
+                        lblmsg.Text = "";
+                        if (gsuite)
+                        {
+                            if (Convert.ToInt32(Session[Constants.SessionName.USERROLEID]) == 4)
+                                Response.Redirect("UserListing.aspx", false);
+                            else
+                                Response.Redirect("ResourcePlanning.aspx", false);
+                        }
+                        else Response.Redirect(returnUrl);
                     }
                 }
                 else
                 {
-                    var application = ConfigurationManager.AppSettings["gSuiteSsoRequest"];
-                    Response.Redirect(application, false);
+                    lblmsg.Text = "Invalid UserName/Password";
                 }
+            }
+            else if (gsuite = ConfigurationManager.AppSettings["gSuiteOnOff"] == "true")
+            {
+                var application = ConfigurationManager.AppSettings["gSuiteSsoRequest"];
+                Response.Redirect(application, false);
             }
         }
         catch (Exception ex)
