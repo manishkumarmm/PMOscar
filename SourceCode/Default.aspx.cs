0d44b1d141cba4a51ba79363b45691e7239a0817
haritha <haritha.es@naicotech.com>
2019-09-25 19:20:08 +0530

    gsuite changes

--- SourceCode/Default.aspx.cs
+++ SourceCode/Default.aspx.cs
@@ -2,6 +2,14 @@
 using PMOscar.Core;
 using PMOscar.Model;
 using PMOscar.DAL;
+using System.Net;
+using System.Web;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.IO;
 
 public partial class Default : System.Web.UI.Page
 {
@@ -11,19 +19,47 @@ public partial class Default : System.Web.UI.Page
     {
         lblmsg.Text = "";
         txtUserName.Focus();
-
         try
         {
+            string encryptString = Request.QueryString["data"];
             string Email = Request.QueryString["Email"];
+            var json_serializer = new JavaScriptSerializer();
+            bool gsuite = false;
+            if (encryptString != null && encryptString != "")
+            {
+                gsuite = true;
+                var request = (HttpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["gSuiteKey"]);
+                var response = (HttpWebResponse)request.GetResponse();
+                var dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
 
+                // Read the content fully up to the end.
+                string secretKey = reader.ReadToEnd();
+
+                TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+                byte[] byteBuff;
+                string iv = Request.QueryString["iv"];
+                desCryptoProvider.Key = Encoding.UTF8.GetBytes(secretKey);
+                desCryptoProvider.IV = UTF8Encoding.UTF8.GetBytes(iv);
+                byteBuff = Convert.FromBase64String(encryptString);
+
+                string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+
+                var gsuiteData = (IDictionary<string, object>)json_serializer.DeserializeObject(plaintext);
+                Email = gsuiteData["email"].ToString();
+            }
             if (Email != null)
             {
-                string Password = PMOscar.BaseDAL.DecryptString(Request.QueryString["Password"].Replace(" ", "+"));
+                string Password = "";
+                if (!gsuite)
+                {
+                    Password = PMOscar.BaseDAL.DecryptString(Request.QueryString["Password"].Replace(" ", "+"));
+                }
                 string returnUrl = Request.QueryString["returnUrl"];
                 var user = new User();
                 var objUserDAL = new UserDAL();
 
-                if (objUserDAL.CheckUserAuthentication(Email, PMOscar.BaseDAL.EncryptText(Password), out user))
+                if (objUserDAL.CheckUserAuthentication(Email, PMOscar.BaseDAL.EncryptText(Password), out user, gsuite))
                 {
                     if (!user.IsActive)
                     {
@@ -36,14 +72,25 @@ public partial class Default : System.Web.UI.Page
                         Session[Constants.SessionName.USERID] = user.UserId;
                         Session[Constants.SessionName.USERROLEID] = user.UserRole.UserRoleId;
                         lblmsg.Text = "";
-                        Response.Redirect(returnUrl);
+                        if (gsuite)
+                        {
+                            if (Convert.ToInt32(Session[Constants.SessionName.USERROLEID]) == 4)
+                                Response.Redirect("UserListing.aspx", false);
+                            else
+                                Response.Redirect("ResourcePlanning.aspx", false);
+                        }
+                        else Response.Redirect(returnUrl);
                     }
-
                 }
                 else
                 {
                     lblmsg.Text = "Invalid UserName/Password";
                 }
+            }
+            else if (gsuite = ConfigurationManager.AppSettings["gSuiteOnOff"] == "true")
+            {
+                var application = ConfigurationManager.AppSettings["gSuiteSsoRequest"];
+                Response.Redirect(application, false);
             }
         }
         catch (Exception ex)
@@ -65,7 +112,7 @@ public partial class Default : System.Web.UI.Page
 
             if (objUserDAL.CheckUserAuthentication(txtUserName.Text, PMOscar.BaseDAL.EncryptText(txtPassword.Text), out user))
             {
-                if(!user.IsActive)
+                if (!user.IsActive)
                 {
                     lblmsg.Text = "This user account has been disabled!";
                 }
@@ -76,11 +123,11 @@ public partial class Default : System.Web.UI.Page
                     Session[Constants.SessionName.USERID] = user.UserId;
                     Session[Constants.SessionName.USERROLEID] = user.UserRole.UserRoleId;
                     lblmsg.Text = "";
-                    if(Convert.ToInt32(Session[Constants.SessionName.USERROLEID])==4)
+                    if (Convert.ToInt32(Session[Constants.SessionName.USERROLEID]) == 4)
                         Response.Redirect("UserListing.aspx");
                     else
-                    Response.Redirect("ResourcePlanning.aspx");
-                }              
+                        Response.Redirect("ResourcePlanning.aspx");
+                }
             }
             else
             {
--- SourceCode/PMOscar.DAL/UserDAL.cs
+++ SourceCode/PMOscar.DAL/UserDAL.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using PMOscar.Model;
 using PMOscar.Core;
+using System.Data;
 
 namespace PMOscar.DAL
 {
@@ -30,41 +31,52 @@ namespace PMOscar.DAL
         /// <param name="password">The password.</param>
         /// <param name="user">The user.</param>
         /// <returns></returns>
-        public bool CheckUserAuthentication(string userName, string password, out User user)
+        public bool CheckUserAuthentication(string userName, string password, out User user, bool gsuite = false)
         {
             var parameters = new List<SqlParameter>();
             parameters.Add(new SqlParameter("@UserName", userName));
             parameters.Add(new SqlParameter("@Password", password));
 
-            var dtUser = ExecuteSPDataTable("CheckUserAuthentication", parameters);
+            var dtUser = new DataTable();
+            if (gsuite)
+            {
+                string query = string.Format("SELECT UserID, FirstName, MiddleName, LastName, UserName,[Password], EmailId, U.UserRoleID,UR.UserRole,IsActive FROM [dbo].[User] U INNER JOIN dbo.UserRole UR ON U.UserRoleID = UR.UserRoleID WHERE UserName = '{0}'", userName);
+
+                dtUser = ExecuteDataTable(query);
+            }
+            else
+            {
+                dtUser = ExecuteSPDataTable("CheckUserAuthentication", parameters);
+            }
+
 
             if (dtUser != null && dtUser.Rows.Count > 0)
             {
                 // Check match case of password
-                if (dtUser.Rows[0][Constants.FieldName.User.PASSWORD].ToString().Equals(password))
+                // if (dtUser.Rows[0][Constants.FieldName.User.PASSWORD].ToString().Equals(password))
                 {
                     user = new User
-                               {
-                                   UserId = Convert.ToInt32(dtUser.Rows[0][Constants.FieldName.User.USERID]),
-                                   UserName = dtUser.Rows[0][Constants.FieldName.User.USERNAME].ToString(),
-                                   Password = dtUser.Rows[0][Constants.FieldName.User.PASSWORD].ToString(),
-                                   FirstName = dtUser.Rows[0][Constants.FieldName.User.FIRSTNAME].ToString(),
-                                   MiddleName = dtUser.Rows[0][Constants.FieldName.User.MIDDLENAME].ToString(),
-                                   LastName = dtUser.Rows[0][Constants.FieldName.User.LASTNAME].ToString(),
-                                   EmailId = dtUser.Rows[0][Constants.FieldName.User.EMAILID].ToString(),
-                                   UserRole =
+                    {
+                        UserId = Convert.ToInt32(dtUser.Rows[0][Constants.FieldName.User.USERID]),
+                        UserName = dtUser.Rows[0][Constants.FieldName.User.USERNAME].ToString(),
+                        Password = dtUser.Rows[0][Constants.FieldName.User.PASSWORD].ToString(),
+                        FirstName = dtUser.Rows[0][Constants.FieldName.User.FIRSTNAME].ToString(),
+                        MiddleName = dtUser.Rows[0][Constants.FieldName.User.MIDDLENAME].ToString(),
+                        LastName = dtUser.Rows[0][Constants.FieldName.User.LASTNAME].ToString(),
+                        EmailId = dtUser.Rows[0][Constants.FieldName.User.EMAILID].ToString(),
+                        UserRole =
                                        new UserRole
-                                           {
-                                               UserRoleId = Convert.ToInt32(dtUser.Rows[0][Constants.FieldName.UserRole.USERROLEID]),
-                                               RoleName   = dtUser.Rows[0][Constants.FieldName.UserRole.USERROLE].ToString()
-                                           },
-                                   IsActive = Convert.ToBoolean(dtUser.Rows[0][Constants.FieldName.User.ISACTIVE])
-                               };
+                                       {
+                                           UserRoleId = Convert.ToInt32(dtUser.Rows[0][Constants.FieldName.UserRole.USERROLEID]),
+                                           RoleName = dtUser.Rows[0][Constants.FieldName.UserRole.USERROLE].ToString()
+                                       },
+                        IsActive = Convert.ToBoolean(dtUser.Rows[0][Constants.FieldName.User.ISACTIVE])
+                    };
 
                     return true;
                 }
             }
-               
+
             user = null;
             return false;
         }
